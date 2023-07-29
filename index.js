123b598fdd271d7b498664eaf54077c1913ffa3e
GuilhermeFPereira <guilll.pereira@gmail.com>
2023-07-28 22:32:27 -0300

    authenticando login regist

--- index.js
+++ index.js
@@ -1,13 +1,57 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose  = require('mongoose')
+const User = require('./models/User')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
-app.use(cors())
+const secret = 'ajhsaldnsajkldalkjdhsjsadsad'
+
+const salt = bcrypt.genSaltSync(10)
+
+app.use(cors({credentials:true,origin:'http://localhost:3000'}))
 app.use(express.json())
 
-app.post('/register', (req, res)=> {
+mongoose.connect('mongodb+srv://personBLog:guill6250730@clustergui.lwjwohm.mongodb.net/?retryWrites=true&w=majority')
+
+app.post('/register', async (req, res)=> {
     const { userName, password } = req.body
-    res.json({requestData:{userName,password}})
+    try{
+        const userDoc = await User.create({
+            userName ,
+             password:bcrypt.hashSync(password,salt) 
+            })
+        res.json(userDoc)
+    }catch(e){
+        res.status(400).json(e)
+    }
+ 
+    
+})
+
+app.post('/login', async (req, res) =>{
+    const {userName, password} = req.body
+    const userDoc = await User.findOne({userName})
+   const passOk = bcrypt.compareSync(password, userDoc.password)
+   
+    if(passOk){
+        // logado
+        jwt.sign({userName,id:userDoc._id}, secret, {}, (err,token)=>{
+            if(err) throw err
+            res.cookie('token', token).json('ok')
+        })
+    //res.json()    
+
+    }else{
+        res.status(400).json('Usuario ou Senha Incorreta')
+    }
+
 })
 
 app.listen(4000)
+
+// mongodb+srv://personBLog:guill6250730@clustergui.lwjwohm.mongodb.net/?retryWrites=true&w=majority
+
+// personBlog
+// senha : guill6250730
