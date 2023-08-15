0c933c04f1e4fa20070969d9895dc5e8cc96b3c2
GuilhermeFPereira <guilll.pereira@gmail.com>
2023-08-14 22:47:09 -0300

    att pro github 3000

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const secret = 'ajhsaldnsajkldalkjdhsjsadsad'
 const port = process.env.PORT || 4000
 
 
-app.use(cors({credentials:true,origin: port}))
+app.use(cors({credentials:true,origin: 'http://localhost:3000'}))
 app.use(express.json())
 app.use(cookieParser())
 app.use('/uploads', express.static(__dirname + '/uploads'))
