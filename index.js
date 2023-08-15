d7661601de297aab611e727ad26b469f713a31e2
GuilhermeFPereira <guilll.pereira@gmail.com>
2023-08-14 21:40:37 -0300

    agora port 4000 correta

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const fs = require('fs')
 
 const salt = bcrypt.genSaltSync(10)
 const secret = 'ajhsaldnsajkldalkjdhsjsadsad'
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
 
 app.use(cors({credentials:true,origin:'http://localhost:3000'}))
@@ -147,4 +147,4 @@ app.get('/post/:id', async (req, res) =>{
    res.json(postDoc)
 })
 
-app.listen(4000)
+app.listen(port, () => console.log('Server is running on port', port))
